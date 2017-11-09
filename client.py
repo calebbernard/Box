8cfad39cdba9ac1fb052255e6809e504e6ccc034
calebbernard <calebbernard@gmail.com>
2017-11-09 16:30:54 -0500

    Client stub

--- client.py
+++ client.py
@@ -1,28 +1,34 @@
 import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5555
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
 
-def sendMsg(s, m):
+def sendMsg(m):
     l = str(len(m))
     while len(l) < 5:
         l = "0" + l;
     s.send(l + m)
 
-def recvMsg(s):
+def recvMsg():
     headerSize = 5
     size = int(s.recv(headerSize))
-    return s.recv(size)
+    if size > 0:
+        return s.recv(size)
+    else:
+        return ""
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-sendMsg(s, "")
-print(recvMsg(s))
-sendMsg(s, "")
-print(recvMsg(s))
-sendMsg(s, "")
-print(recvMsg(s))
-sendMsg(s, "")
-print(recvMsg(s))
+def get(m):
+    sendMsg(m)
+    return recvMsg()
+
+def g(m):
+    reply = get(m)
+    print reply
+    return reply
+
+# client code goes here
+
 s.close()
 
 exit()
